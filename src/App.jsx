5ebb3c20270cc71e2d911497487dedbff2f8727d
maxiamikel19 <maxloversist19@gmail.com>
2024-09-20 18:28:29 -0300

    Impl funcionalidades para completar e deletar task

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       name: "Task 002",
       description: "Iniciar a task 002",
       category: "Work",
-      isCompleted: true,
+      isCompleted: false,
     },
   ]);
 
@@ -34,15 +34,41 @@ function App() {
     ];
     setTasks(newTask);
   };
+  const removeTask = (id) => {
+    const arrTasks = [...tasks];
+    const filterArrTasks = arrTasks.filter((task) =>
+      task.id !== id ? task : null
+    );
+    setTasks(filterArrTasks);
+  };
+
+  const completeTask = (id) => {
+    const arrTasks = [...tasks];
+    arrTasks.map((task) =>
+      task.id === id ? (task.isCompleted = !tasks.isCompleted) : tasks
+    );
+    setTasks(arrTasks);
+  };
   return (
     <div className="app">
       <h1>
         Tasks list<span className="task-number">({tasks.length})</span>
       </h1>
       <div className="task-list">
-        {tasks.map((task) => (
-          <Task key={task.id} task={task} />
-        ))}
+        {tasks.length > 0 ? (
+          tasks.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              removeTask={removeTask}
+              completeTask={completeTask}
+            />
+          ))
+        ) : (
+          <div className="no-task">
+            <h1>No tasks to show</h1>
+          </div>
+        )}
       </div>
       <TaskForm addNewTask={addNewTask} />
     </div>
--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -2,21 +2,35 @@ import { FaCheck } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 import "./Task.css";
 
-function Task({ task }) {
+function Task({ task, removeTask, completeTask }) {
   const { id, name, description, category, isCompleted } = task;
 
   return (
     <div className="task">
       <div className="task-content">
-        <p className="task-content-name">{name}</p>
+        <div className="task-header">
+          <p className="task-content-name">{name}</p>
+          <p className="task-metadata">
+            <span>Status</span>
+            <span
+              className={`${!isCompleted ? "task-pending" : "task-complete"}`}
+            >
+              {!isCompleted ? "Pending" : "Finish"}
+            </span>
+          </p>
+        </div>
         <p>{description}</p>
         <p>{category}</p>
+        <p>{isCompleted}</p>
       </div>
       <div className="actions">
-        <button className="action-complete action">
+        <button
+          className="action-complete action"
+          onClick={() => completeTask(id)}
+        >
           <FaCheck />
         </button>
-        <button className="action-delete action">
+        <button className="action-delete action" onClick={() => removeTask(id)}>
           <AiFillDelete />
         </button>
       </div>
